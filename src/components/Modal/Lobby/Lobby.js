625e5313ed1365f26d116b3061d0fd3d7bff3b02
Erfan <soulburn007@gamil.com>
2020-03-15 01:38:21 +0330

    🧱🧹Clean up lobby

--- src/components/Modal/Lobby/Lobby.js
+++ src/components/Modal/Lobby/Lobby.js
@@ -1,21 +1,8 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import styles from './Lobby.module.css';
-import axios from 'axios';
 
 function Lobby(props) {
-  let data = useRef();
-
-  useEffect(() => {
-    const getLobby = async () => {
-      const response = await axios.get(
-        'https://tortugack.herokuapp.com/api/v1/lobby'
-      );
-      return response.data;
-    };
-    data.current = getLobby();
-  }, []);
-
-  const lobbies = data.current.lobbies.map(lobby => (
+  const lobbies = props.data.lobbies.map(lobby => (
     <>
       <p>id: {lobby.id}</p>
       <p>size: {lobby.size}</p>
