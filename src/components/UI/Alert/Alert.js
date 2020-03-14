41c59d012cad267c2021756ff18695386633d308
Erfan <soulburn007@gamil.com>
2020-03-14 20:05:38 +0330

    implemented Alert componenet

--- src/components/UI/Alert/Alert.js
+++ src/components/UI/Alert/Alert.js
@@ -2,7 +2,15 @@ import React from 'react';
 import styles from './Alert.module.css';
 
 function Alert(props) {
-  return <div className={styles.Alert}>Hey</div>;
+  const classes = [styles.Alert];
+  if (!props.data.show) classes.push(styles.Closed);
+
+  return (
+    <div className={classes.join(' ')}>
+      <p>{props.data.message}</p>
+      <button onClick={props.hideAlert}>x</button>
+    </div>
+  );
 }
 
 export default Alert;
