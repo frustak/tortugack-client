847aa5e23bec276cd97fc8de8325ce7578fc0003
Frostack <soulburn007@gmail.com>
2020-03-18 11:35:17 +0330

    🧱Add autofocus & enter handler

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 function SignIn(props) {
+  const enterKeyPress = event => {
+    if (event.key === 'Enter') props.login();
+  };
+
   return (
     <>
       <input
         type="text"
         placeholder="username"
+        autoFocus
         onChange={props.usernameHandler}
+        onKeyUp={enterKeyPress}
       />
       <button onClick={props.login}>login</button>
     </>
