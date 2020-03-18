937f495387a43ed296a342cda4a1789fc4269dab
Frostack <soulburn007@gmail.com>
2020-03-18 19:26:38 +0330

    🧱Add Game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './Game.module.css';
+import GameMap from './GameMap/GameMap';
+import GameInfo from './GameInfo/GameInfo';
 
 function Game(props) {
-  return <></>;
+  return (
+    <div className={styles.Game}>
+      <div>
+        <GameMap />
+      </div>
+      <div>
+        <GameInfo data={props.data} />
+      </div>
+    </div>
+  );
 }
 
 export default Game;
