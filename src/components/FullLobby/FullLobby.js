9a87d60e1aaa886b43ca0d683e13436efebd2ec2
Frostack <soulburn007@gmail.com>
2020-03-18 13:35:44 +0330

    🧱Show start button only for host

--- src/components/FullLobby/FullLobby.js
+++ src/components/FullLobby/FullLobby.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Lobby from '../Lobbies/Lobby/Lobby';
 
 function FullLobby(props) {
+  let output = null;
+  if (props.data.host.username === props.username) {
+    output = <button onClick={props.start}>start</button>;
+  }
+
   return (
     <>
       <Lobby data={props.data} />
-      <button onClick={props.start}>start</button>
+      {output}
       <button onClick={props.leave}>leave</button>
     </>
   );
--- src/components/Lobbies/Lobby/Lobby.js
+++ src/components/Lobbies/Lobby/Lobby.js
@@ -14,8 +14,7 @@ function Lobby(props) {
       <p>size: {data.size}</p>
       <p>occupy: {data.occupy}</p>
       <p>host: {data.host.username}</p>
-      <p>game started: {data.game_started}</p>
-      <p>players: </p>
+      <p>game started: {data.game_started.toString()}</p>
       {players}
     </>
   );
