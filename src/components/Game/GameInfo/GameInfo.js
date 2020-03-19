dbcb84e81a02a85cfdb89790f00c3f4c6563b9a0
Frostack <soulburn007@gmail.com>
2020-03-19 23:09:15 +0330

    Add player names and coloring in game

--- src/components/Game/GameInfo/GameInfo.js
+++ src/components/Game/GameInfo/GameInfo.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import _ from 'lodash';
+import styles from './GameInfo.module.css';
 
 function GameInfo(props) {
+  let hostColor;
+
+  const players = _.keys(props.data.players_position).map((player, index) => {
+    const style = styles[`Player_${index + 1}`];
+    if (props.data.turn.username === player) {
+      hostColor = style;
+      return (
+        <p className={style}>
+          <strong>{player}</strong>
+        </p>
+      );
+    }
+    return <p className={style}>{player}</p>;
+  });
+
   return (
     <>
-      <p>turn: {props.data.turn.username}</p>
+      <p className={hostColor}>turn: {props.data.turn.username}</p>
       <p>team: {props.data.player_game_info.team}</p>
+      {players}
     </>
   );
 }
 
 export default GameInfo;
-
