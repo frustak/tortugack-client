cc2ee8943522f5adeff0cf28d864e109ce19da79
Frostack <soulburn007@gmail.com>
2020-03-20 14:50:41 +0330

    🧱Add GameAction component (mock up)

---
+++ src/components/Game/GameAction/GameAction.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './GameAction.module.css';
+
+function GameAction(props) {
+  return (
+    <div className={styles.GameAction}>
+      <button>btn</button>
+      <button>btn</button>
+      <button>btn</button>
+      <button>btn</button>
+    </div>
+  );
+}
+
+export default GameAction;
