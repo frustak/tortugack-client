dfc398a4773998ae6b8fb4690c446d97a9c30688
Frostack <soulburn007@gmail.com>
2020-03-20 14:51:45 +0330

    🌌Improve GameInfo component

--- src/components/Game/GameInfo/GameInfo.js
+++ src/components/Game/GameInfo/GameInfo.js
@@ -5,7 +5,7 @@ import styles from './GameInfo.module.css';
 function GameInfo(props) {
   let hostColor;
 
-  const players = _.keys(props.data.players_position).map((player, index) => {
+  const players = _.keys(props.data.playersPosition).map((player, index) => {
     const style = styles[`Player_${index + 1}`];
     if (props.data.turn.username === player) {
       hostColor = style;
@@ -19,11 +19,12 @@ function GameInfo(props) {
   });
 
   return (
-    <>
+    <div className={styles.GameInfo}>
       <p className={hostColor}>turn: {props.data.turn.username}</p>
-      <p>team: {props.data.player_game_info.team}</p>
+      <p>team: {props.data.playerGameInfo.team}</p>
+      <p>players: </p>
       {players}
-    </>
+    </div>
   );
 }
 
