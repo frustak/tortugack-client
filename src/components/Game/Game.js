1117e9d3fa104c0e3ef3353538573feba3a94241
Frostack <soulburn007@gmail.com>
2020-03-20 14:46:15 +0330

    🌈🌱Implement Game style

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import styles from './Game.module.css';
 import GameMap from './GameMap/GameMap';
 import GameInfo from './GameInfo/GameInfo';
+import GameAction from './GameAction/GameAction';
 
 function Game(props) {
   return (
-    <>
+    <div className={styles.Game}>
       <GameMap data={props.data} />
       <GameInfo data={props.data} />
       <GameAction />
