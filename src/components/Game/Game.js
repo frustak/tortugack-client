20a802930c5a186e20a97a5816365659e3eeae65
Frostack <soulburn007@gmail.com>
2020-03-21 17:12:59 +0430

    🌧Change props name

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,13 +3,19 @@ import styles from './Game.module.css';
 import GameMap from './GameMap/GameMap';
 import GameInfo from './GameInfo/GameInfo';
 import GameAction from './GameAction/GameAction';
+import VoteCards from './VoteCards/VoteCards';
 
 function Game(props) {
   return (
     <div className={styles.Game}>
-      <GameMap data={props.data} />
-      <GameInfo data={props.data} />
-      <GameAction />
+      <GameMap data={props.data.gameStatus} />
+      <GameInfo data={props.data.gameStatus} username={props.username} />
+      <VoteCards data={props.data.gameStatus} />
+      <GameAction
+        data={props.data.gameStatus}
+        sendAction={props.sendAction}
+        username={props.username}
+      />
     </div>
   );
 }
