97da87d051e2812f84212e34ef82d3d6e69efa0a
Frostack <soulburn007@gmail.com>
2020-03-21 17:10:54 +0430

    🌌Improve output

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Loading.module.css';
 
 function Loading(props) {
-  let output = <></>;
+  let output = null;
   if (props.show) output = <div className={styles.Loading}>loading...</div>;
   return output;
 }
