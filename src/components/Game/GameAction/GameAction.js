f85a7d4a836c4c2f1450774b481eb99d76678b3f
Frostack <soulburn007@gmail.com>
2020-03-21 17:13:31 +0430

    🧱🌌Imrpove GameAction compoenent

--- src/components/Game/GameAction/GameAction.js
+++ src/components/Game/GameAction/GameAction.js
@@ -2,14 +2,31 @@ import React from 'react';
 import styles from './GameAction.module.css';
 
 function GameAction(props) {
-  return (
-    <div className={styles.GameAction}>
-      <button>btn</button>
-      <button>btn</button>
-      <button>btn</button>
-      <button>btn</button>
-    </div>
+  let output = props.data.playerGameInfo.availableActions.map(
+    (action, index) => (
+      <button onClick={() => props.sendAction(action)} key={index}>
+        {action}
+      </button>
+    )
   );
+
+  const vote = () => {
+    const voteIndex = prompt('gimme vote');
+    const action = 'vote';
+    const payload = {
+      voteCardIndex: +voteIndex,
+    };
+
+    props.sendAction(action, payload);
+  };
+
+  const canVote = props.data.lastAction?.actionData.participatingPlayers.some(
+    player => props.username === player
+  );
+
+  if (canVote) output.push(<button onClick={vote}>vote</button>);
+
+  return <div className={styles.GameAction}>{output}</div>;
 }
 
 export default GameAction;
