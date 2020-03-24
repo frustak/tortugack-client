e5e82f2aee33fbb7ba65930a4b6c46b2532f45b0
Frostack <soulburn007@gmail.com>
2020-03-24 09:35:12 +0430

    🌈Implement material-ui in sign in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import styles from './SignIn.module.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    marginBottom: '8px',
+  },
+});
 
 function SignIn(props) {
   const enterKeyPress = event => {
     if (event.key === 'Enter') props.login();
   };
 
+  const classes = useStyles();
   return (
-    <>
-      <input
-        type="text"
-        placeholder="username"
-        autoFocus
-        onChange={props.usernameHandler}
-        onKeyUp={enterKeyPress}
-      />
-      <button onClick={props.login}>login</button>
-    </>
+    <div className={styles.SignIn}>
+      <div className={styles.Content}>
+        <TextField
+          type="text"
+          autoFocus
+          onChange={props.usernameHandler}
+          onKeyUp={enterKeyPress}
+          variant="outlined"
+          label="Username"
+          color="primary"
+          className={classes.root}
+        />
+        <Button variant="contained" color="primary" onClick={props.login}>
+          login
+        </Button>
+      </div>
+    </div>
   );
 }
 
