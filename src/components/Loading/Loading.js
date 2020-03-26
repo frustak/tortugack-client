f65331d5a437e293a477ace759dbc0223ec1d61d
Frostack <soulburn007@gmail.com>
2020-03-26 08:42:13 +0430

    🧱Loading component is circle now

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './Loading.module.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function Loading(props) {
   let output = null;
-  if (props.show) output = <div className={styles.Loading}>loading...</div>;
+  if (props.show) output = <CircularProgress className={styles.Loading} />;
   return output;
 }
 
