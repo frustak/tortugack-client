6af77363e0263f56136a690e07d712c94af39ceb
Frostack <soulburn007@gmail.com>
2020-03-26 08:46:50 +0430

    🌌🧱Imrpove FullLobby component

--- src/components/FullLobby/FullLobby.js
+++ src/components/FullLobby/FullLobby.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import Lobby from '../Lobbies/Lobby/Lobby';
+import Button from '@material-ui/core/Button';
+import styles from './FullLobby.module.css';
 
 function FullLobby(props) {
   let output = null;
   if (props.data.canStart) {
-    output = <button onClick={props.start}>start</button>;
+    output = (
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={props.start}
+        className={styles.Button}
+      >
+        start
+      </Button>
+    );
   }
 
   return (
-    <>
+    <div className={styles.FullLobby}>
       <Lobby data={props.data.lobby} />
       {output}
-      <button onClick={props.leave}>leave</button>
-    </>
+      <Button
+        variant="outlined"
+        color="secondary"
+        onClick={props.leave}
+        className={styles.Button}
+      >
+        leave
+      </Button>
+    </div>
   );
 }
 
