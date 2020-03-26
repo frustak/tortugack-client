388208d779cc932e99a4e13c135faea5800dbe24
Frostack <soulburn007@gmail.com>
2020-03-26 08:44:29 +0430

    🌌🧱Imrpove GameInfo component

--- src/components/Game/GameInfo/GameInfo.js
+++ src/components/Game/GameInfo/GameInfo.js
@@ -14,25 +14,36 @@ function GameInfo(props) {
       output = <strong>{player}</strong>;
     }
     if (props.username === player) userColor = style;
+    let divider = ',';
+    if (index === _.keys(props.data.playersPosition).length - 1) divider = '';
     return (
-      <p className={style} key={index}>
+      <span className={style} key={index}>
         {output}
-      </p>
+        {divider}&nbsp;
+      </span>
     );
   });
 
   return (
     <div className={styles.GameInfo}>
-      <p>
-        your name: <span className={userColor}>{props.username}</span>
-      </p>
-      <p>your team: {props.data.playerGameInfo.team}</p>
-      <p>your role: {props.data.playerGameInfo.role}</p>
-      <p>
-        turn: <span className={hostColor}>{props.data.turn.username}</span>
-      </p>
-      <p>players: </p>
-      {players}
+      <div className={styles.col}>
+        <span>Name</span>
+        <span>Team </span>
+        <span>Role </span>
+        <span>Turn</span>
+        <span>Players</span>
+      </div>
+      <div className={styles.col}>
+        <span className={userColor}>{props.username}</span>
+        <span>{props.data.playerGameInfo.team}</span>
+        <span>
+          {props.data.playerGameInfo.role
+            ? props.data.playerGameInfo.role
+            : 'not defined'}
+        </span>
+        <span className={hostColor}>{props.data.turn.username}</span>
+        <span className={styles.row}>{players}</span>
+      </div>
     </div>
   );
 }
