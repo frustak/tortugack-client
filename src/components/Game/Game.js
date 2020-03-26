9d8d7bcc47b773745bf1dfa4792869f73cb58c70
Frostack <soulburn007@gmail.com>
2020-03-26 08:46:21 +0430

    🌌🧱Imrpove Game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Game.module.css';
 import GameMap from './GameMap/GameMap';
 import GameInfo from './GameInfo/GameInfo';
@@ -6,16 +6,32 @@ import GameAction from './GameAction/GameAction';
 import VoteCards from './VoteCards/VoteCards';
 
 function Game(props) {
+  const [voteClickable, setVoteClickable] = useState(false);
+
   return (
     <div className={styles.Game}>
-      <GameMap data={props.data.gameStatus} />
+      <div className={styles.row}>
+        <GameMap data={props.data.gameStatus} />
+        <GameAction
+          data={props.data.gameStatus}
+          sendAction={props.sendAction}
+          username={props.username}
+          showModal={props.showModal}
+          moveHandler={props.moveHandler}
+          putChestHandler={props.putChestHandler}
+          setVoteClickable={setVoteClickable}
+          moveChestHandler={props.moveChestHandler}
+          maroonHandler={props.maroonHandler}
+        />
+        <VoteCards
+          data={props.data.gameStatus}
+          show={props.modal}
+          close={props.closeModal}
+          vote={props.vote}
+          clickable={voteClickable}
+        />
+      </div>
       <GameInfo data={props.data.gameStatus} username={props.username} />
-      <VoteCards data={props.data.gameStatus} />
-      <GameAction
-        data={props.data.gameStatus}
-        sendAction={props.sendAction}
-        username={props.username}
-      />
     </div>
   );
 }
