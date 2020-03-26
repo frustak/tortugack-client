ba68239a045220835c0236d7e8385ad4572b0570
Frostack <soulburn007@gmail.com>
2020-03-26 22:33:40 +0430

    🧰Improve chest model

--- src/components/Game/GameMap/Cell/Cell.js
+++ src/components/Game/GameMap/Cell/Cell.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import styles from './Cell.module.css';
+import chestIcon from '../../../../assets/icons/chest.png';
 
 function Cell(props) {
   const classes = [styles.Cell, styles[props.color]];
 
-  return (
-    <div
-      className={classes.join(' ')}
-      style={{ left: props.position.x - 16, top: props.position.y - 16 }}
-    >
-      {props.player}
-    </div>
-  );
+  if (props.color !== 'Chest') {
+    return (
+      <div
+        className={classes.join(' ')}
+        style={{ left: props.position.x - 16, top: props.position.y - 16 }}
+      >
+        {props.player}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={classes.join(' ')}
+        style={{ left: props.position.x - 16, top: props.position.y - 16 }}
+      >
+        <img className={styles.Image} src={chestIcon} alt="" />
+        {props.player}
+      </div>
+    );
+  }
 }
 
 export default Cell;
