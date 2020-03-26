adabc38dde7dbff1e7479a52d5424e5e2dddda70
Frostack <soulburn007@gmail.com>
2020-03-26 08:41:12 +0430

    🧱Improve Lobby component

--- src/components/Lobbies/Lobby/Lobby.js
+++ src/components/Lobbies/Lobby/Lobby.js
@@ -1,23 +1,64 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import styles from './Lobby.module.css';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    borderRadius: '0 0 4px 4px',
+  },
+});
 
 function Lobby(props) {
   const data = props.data;
-  const players = data.players.map((player, index) => (
-    <p key={index}>
-      player {index + 1}: {player.username}
-    </p>
-  ));
+  const players = data.players.map((player, index) => {
+    if (data.players.length - 1 === index) return `${player.username}`;
+    return `${player.username}, `;
+  });
+
+  const btnClasses = useStyles();
+  let output = null;
+  if (props.showBtn)
+    output = (
+      <Button
+        variant="outlined"
+        color="primary"
+        size="small"
+        classes={btnClasses}
+        onClick={() => props.joinLobby(data.id)}
+      >
+        join
+      </Button>
+    );
 
   return (
-    <>
-      <p>id: {data.id}</p>
-      <p>size: {data.size}</p>
-      <p>occupy: {data.occupy}</p>
-      <p>host: {data.host.username}</p>
-      <p>game started: {data.gameStarted.toString()}</p>
-      {players}
-      <hr />
-    </>
+    <Paper className={styles.m}>
+      <div className={styles.Lobby}>
+        <div className={styles.row}>
+          <div className={styles.colBorder}>
+            <span>Lobby ID</span>
+            <span>Size</span>
+            <span>Occupied</span>
+            <span>Host</span>
+            <span>Game Status</span>
+            <span>Players</span>
+          </div>
+          <div className={styles.col}>
+            <span>{data.id}</span>
+            <span>{data.size}</span>
+            <span>{data.occupy}</span>
+            <span>{data.host.username}</span>
+            <span>{data.gameStarted ? 'in game' : 'in lobby'}</span>
+            <span>
+              <div className={styles.Players}>{players}</div>
+            </span>
+          </div>
+        </div>
+      </div>
+      {output}
+    </Paper>
   );
 }
 
