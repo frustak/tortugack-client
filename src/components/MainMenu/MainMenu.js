a745e75ac914c98aae2afdb702d12c224b01251d
Frostack <soulburn007@gmail.com>
2020-03-26 08:40:32 +0430

    🌌🧱Imrpove MainMenu component

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import styles from './MainMenu.module.css';
 
 function MainMenu(props) {
   return (
-    <>
-      <p>welcome {props.username}</p>
-      <button onClick={props.showLobbies}>lobbies</button>
-      <button onClick={props.joinLobby}>join by id</button>
-      <button onClick={props.createLobby}>create game</button>
-      <button onClick={props.logout}>sign out</button>
-    </>
+    <div className={styles.MainMenu}>
+      <div className={styles.Content}>
+        <p>Welcome {props.username}</p>
+        <Button variant="outlined" color="primary" onClick={props.showLobbies}>
+          lobbies
+        </Button>
+        <Button variant="outlined" color="primary" onClick={props.joinLobby}>
+          join by id
+        </Button>
+        <Button variant="outlined" color="primary" onClick={props.createLobby}>
+          create game
+        </Button>
+        <Button variant="outlined" color="secondary" onClick={props.logout}>
+          sign out
+        </Button>
+      </div>
+    </div>
   );
 }
 
