5f8507c100abc62b32229d291dc5db90d053e28c
Frostack <soulburn007@gmail.com>
2020-03-26 08:41:42 +0430

    🌌🧱Imrpove Lobbies component

--- src/components/Lobbies/Lobbies.js
+++ src/components/Lobbies/Lobbies.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Lobby from './Lobby/Lobby';
+import styles from './Lobbies.module.css';
+import Button from '@material-ui/core/Button';
 
 function Lobbies(props) {
   const lobbies = props.lobbies.map(lobby => (
-    <Lobby data={lobby} key={lobby.id} />
+    <Lobby data={lobby} key={lobby.id} showBtn joinLobby={props.joinLobby}/>
   ));
 
   return (
     <>
-      <button onClick={props.back}>back</button>
-      <button onClick={props.refresh}>refresh</button>
-      <p>lobbies:</p>
-      <hr />
-      {lobbies}
+      <div className={styles.Buttons}>
+        <Button color="primary" onClick={props.refresh}>
+          refresh
+        </Button>
+        <Button color="secondary" onClick={props.back}>
+          back
+        </Button>
+      </div>
+      <div className={styles.Lobbies}>{lobbies}</div>
     </>
   );
 }
