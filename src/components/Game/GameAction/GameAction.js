db915a0808ea7e867715275990c85bb64779fbb1
Frostack <soulburn007@gmail.com>
2020-03-26 08:45:45 +0430

    🌌🧱Imrpove GameAction component

--- src/components/Game/GameAction/GameAction.js
+++ src/components/Game/GameAction/GameAction.js
@@ -1,96 +1,65 @@
 import React from 'react';
 import styles from './GameAction.module.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    margin: '2px',
+  },
+});
 
 function GameAction(props) {
-  const vote = () => {
-    const voteIndex = prompt('gimme vote');
-    const payload = { voteCardIndex: +voteIndex };
-    props.sendAction('vote', payload);
-  };
-
-  const putChest = () => {
-    const lastAction = props.data.lastAction;
-    const payload = {};
-    if (
-      lastAction?.actionType === 'call for an attack' &&
-      lastAction?.actionData.whichCaptain.username === props.username &&
-      lastAction?.actionData.state === 'success' &&
-      lastAction?.actionData.fromOtherShip
-    ) {
-      payload.fromWhichTeam = prompt(
-        'which side you want to steal a chest? 1) BRITAIN, 2) FRANCE'
-      );
-    }
-    const choice = prompt(
-      'which side you want to put the chest? 1) BRITAIN, 2) FRANCE'
-    );
-    if (choice === '1') payload.whichTeam = 'BRITAIN';
-    if (choice === '2') payload.whichTeam = 'FRANCE';
-    props.sendAction('put chest', payload);
-  };
-
-  const maroonCrew = () => {
-    const crewToMaroon = prompt('who do you want to maroon?');
-    props.sendAction('maroon any crew mate to tortuga', { crewToMaroon });
-  };
-
-  const move = () => {
-    let moveWhere;
-    const position = props.data.playersPosition[props.username];
-    if (position.startsWith('jr')) moveWhere = 'jr_b';
-    if (position.startsWith('fd')) moveWhere = 'fd_b';
-    if (position.startsWith('tr')) {
-      const input = prompt(
-        'where you wanna go? 1) jolly roger boat, 2) flying dutchman boat'
-      );
-      if (input === '1') moveWhere = 'jr_b';
-      if (input === '2') moveWhere = 'fd_b';
-    }
-    if (position === 'jr_b') {
-      const input = prompt(
-        'where you wanna go? 1) tortuga island, 2) jolly roger ship'
-      );
-      if (input === '1') moveWhere = 'tr';
-      if (input === '2') moveWhere = 'jr';
-    }
-    if (position === 'fd_b') {
-      const input = prompt(
-        'where you wanna go? 1) tortuga island, 2) flying dutchman ship'
-      );
-      if (input === '1') moveWhere = 'tr';
-      if (input === '2') moveWhere = 'fd';
-    }
-    props.sendAction('move', { moveWhere });
-  };
-
-  const stealChest = () => {
-    const choice = prompt(
-      'which team you want to steal from? 1) britain, 2) france'
-    );
-    let fromHold;
-    if (choice === '1') fromHold = 'BRITAIN';
-    if (choice === '2') fromHold = 'FRANCE';
-    props.sendAction('move treasure', { fromHold });
-  };
+  const classes = useStyles();
 
   const output = props.data.playerGameInfo.availableActions.map(
     (action, index) => {
       let click = () => props.sendAction(action);
-      if (action === 'vote') click = vote;
-      if (action === 'put chest') click = putChest;
-      if (action === 'maroon any crew mate to tortuga') click = maroonCrew;
-      if (action === 'move') click = move;
-      if (action === 'move treasure') click = stealChest;
+      if (action === 'put chest') click = props.putChestHandler;
+      if (action === 'maroon any crew mate to tortuga')
+        click = props.maroonHandler;
+      if (action === 'move') click = props.moveHandler;
+      if (action === 'move treasure') click = props.moveChestHandler;
+      if (action === 'vote') {
+        click = props.showModal;
+        props.setVoteClickable(true);
+      }
 
       return (
-        <button onClick={click} key={index}>
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={click}
+          key={index}
+          classes={classes}
+        >
           {action}
-        </button>
+        </Button>
       );
     }
   );
 
-  return <div className={styles.GameAction}>{output}</div>;
+  let voteBtn = (
+    <Button
+      variant="outlined"
+      size="small"
+      onClick={props.showModal}
+      classes={classes}
+    >
+      vote cards
+    </Button>
+  );
+  if (
+    props.data.playerGameInfo.availableActions.some(action => action === 'vote')
+  )
+    voteBtn = null;
+
+  return (
+    <div className={styles.GameAction}>
+      {output}
+      {voteBtn}
+    </div>
+  );
 }
 
 export default GameAction;
