6f3a75616da6e5c2fedebf5af7bf935d4bb81118
Frostack <soulburn007@gmail.com>
2020-04-05 21:12:11 +0430

    🌌Change continuous if statements to switch statement

--- src/components/Game/GameAction/GameAction.js
+++ src/components/Game/GameAction/GameAction.js
@@ -12,6 +12,8 @@ import attackIcon from '../../../assets/icons/attack.png';
 import maroonIcon from '../../../assets/icons/maroon.png';
 import moveChestIcon from '../../../assets/icons/move-chest.png';
 import voteCardIcon from '../../../assets/icons/vote-card.png';
+import useCardIcon from '../../../assets/icons/use-card.png';
+import keepCardIcon from '../../../assets/icons/keep-card.png';
 
 const useStyles = makeStyles({
   root: {
@@ -29,42 +31,53 @@ function GameAction(props) {
       let icon;
       let click = () => props.sendAction(action);
 
-      if (action === 'put chest') click = props.putChestHandler;
-      if (action === 'maroon any crew mate to tortuga') {
-        click = props.maroonHandler;
-        icon = maroonIcon;
-      }
-      if (action === 'move') {
-        click = props.moveHandler;
-        icon = moveIcon;
-      }
-      if (action === 'move treasure') {
-        click = props.moveChestHandler;
-        icon = moveChestIcon;
-      }
-      if (action === 'vote') {
-        click = props.showModal;
-        icon = voteCardIcon;
-        props.setVoteClickable(true);
-      }
-      if (action === 'view two event cards') {
-        click = props.viewTwoCardsHandler;
-        icon = twoEventIcon;
-      }
-      if (action === 'reveal one event card') {
-        icon = cardIcon;
-      }
-      if (action === 'force another player to choose card') {
-        icon = forceCardIcon;
-      }
-      if (action === 'call for a mutiny') {
-        icon = mutinyIcon;
-      }
-      if (action === 'call for an attack') {
-        icon = attackIcon;
-      }
-      if (action === 'put chest') {
-        icon = moveChestIcon;
+      switch (action) {
+        case 'put chest':
+          icon = moveChestIcon;
+          click = props.putChestHandler;
+          break;
+        case 'maroon any crew mate to tortuga':
+          click = props.maroonHandler;
+          icon = maroonIcon;
+          break;
+        case 'move':
+          click = props.moveHandler;
+          icon = moveIcon;
+          break;
+        case 'move treasure':
+          click = props.moveChestHandler;
+          icon = moveChestIcon;
+          break;
+        case 'vote':
+          click = props.showModal;
+          icon = voteCardIcon;
+          props.setVoteClickable(true);
+          break;
+        case 'view two event cards':
+          click = props.viewTwoCardsHandler;
+          icon = twoEventIcon;
+          break;
+        case 'reveal one event card':
+          click = props.revealCardHandler;
+          icon = cardIcon;
+          break;
+        case 'force another player to choose card':
+          icon = forceCardIcon;
+          break;
+        case 'call for a mutiny':
+          icon = mutinyIcon;
+          break;
+        case 'call for an attack':
+          icon = attackIcon;
+          break;
+        case 'USE-EVENT-CARD':
+          icon = useCardIcon;
+          click = props.sendAction('SEE-EVENT-CARD-OPTIONS');
+          break;
+        case 'KEEP-EVENT-CARD':
+          icon = keepCardIcon;
+          break;
+        default:
       }
 
       return (
