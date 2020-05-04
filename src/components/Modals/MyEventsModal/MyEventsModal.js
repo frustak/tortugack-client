c30bffe976102faa8104b78181c48a2208cee643
Frostack <soulburn007@gmail.com>
2020-05-04 12:36:37 +0430

    🔧 fix event card data handling

--- src/components/Modals/MyEventsModal/MyEventsModal.js
+++ src/components/Modals/MyEventsModal/MyEventsModal.js
@@ -2,17 +2,18 @@ import React from 'react';
 import RevealedEventCard from '../TwoEventCardsModal/RevealedEventCard/RevealedEventCard';
 
 function MyEventsModal({ cards, close, eventCardHandler }) {
-  const clickHandler= (slug) => {
+  const clickHandler = slug => {
     eventCardHandler(slug);
     close();
-  }
-  
+  };
+
   const output = cards.map(card => (
     <RevealedEventCard
-      imageUrl={card.imageUrl}
-      title={card.title}
-      description={card.description}
-      click={() => clickHandler(card.slug)}
+      imageUrl={card.eventCard.imageUrl}
+      title={card.eventCard.title}
+      description={card.eventCard.description}
+      click={() => clickHandler(card.eventCard.slug)}
+      isDisabled={!card.canUse}
     />
   ));
   return output;
