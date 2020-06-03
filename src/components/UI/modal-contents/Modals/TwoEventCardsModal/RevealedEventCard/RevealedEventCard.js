65bd65fbc7713f55938122fe514a2f4d84d3d767
Frostack <soulburn007@gmail.com>
2020-06-03 13:56:00 +0430

    🐛 fix use event card button

--- src/components/UI/modal-contents/Modals/TwoEventCardsModal/RevealedEventCard/RevealedEventCard.js
+++ src/components/UI/modal-contents/Modals/TwoEventCardsModal/RevealedEventCard/RevealedEventCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Card, makeStyles, Button } from '@material-ui/core';
-import { connect } from 'react-redux';
 
 const useStyles = makeStyles({
   card: {
@@ -37,7 +36,7 @@ function RevealedEventCard(props) {
         <p className={classes.small}>{props.title}</p>
         <p className={classes.smallest}>{props.description}</p>
       </Card>
-      {props.showButton && props.isPlayerTurn && props.isNotInAction ? (
+      {props.showButton ? (
         <Button
           className={classes.btn}
           variant="contained"
@@ -52,14 +51,4 @@ function RevealedEventCard(props) {
   );
 }
 
-const mapStateToProps = state => {
-  return {
-    isPlayerTurn:
-      state.game.data.gameStatus.turn.username === state.user.username,
-    isNotInAction:
-      state.game.data.gameStatus.lastAction.actionType !==
-      'reveal one event card',
-  };
-};
-
-export default connect(mapStateToProps)(RevealedEventCard);
+export default RevealedEventCard;
