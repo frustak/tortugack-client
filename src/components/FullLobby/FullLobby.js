c577fca9957a9d7c6d137417bff592e16e250524
Frostack <soulburn007@gmail.com>
2020-06-03 11:56:58 +0430

    🧱 implement FullLobby component

--- src/components/FullLobby/FullLobby.js
+++ src/components/FullLobby/FullLobby.js
@@ -1,37 +1,66 @@
 import React from 'react';
 import Lobby from '../Lobbies/Lobby/Lobby';
 import Button from '@material-ui/core/Button';
-import styles from './FullLobby.module.css';
+import { connect } from 'react-redux';
 
-function FullLobby(props) {
-  let output = null;
-  if (props.data.canStart) {
-    output = (
-      <Button
-        variant="outlined"
-        color="primary"
-        onClick={props.start}
-        className={styles.Button}
-      >
-        start
-      </Button>
-    );
+import styles from './FullLobby.module.css';
+import {
+  startLobbyPolling,
+  leaveLobby,
+  endLobbyPolling,
+  startGame,
+} from '../../actions';
+
+class FullLobby extends React.Component {
+  componentDidMount() {
+    this.props.startLobbyPolling();
   }
 
-  return (
-    <div className={styles.FullLobby}>
-      <Lobby data={props.data.lobby} />
-      {output}
-      <Button
-        variant="outlined"
-        color="secondary"
-        onClick={props.leave}
-        className={styles.Button}
-      >
-        leave
-      </Button>
-    </div>
-  );
+  componentWillUnmount() {
+    this.props.endLobbyPolling();
+  }
+
+  render() {
+    if (!this.props.lobbyData || !this.props.lobbyData.lobby) return null;
+
+    let output = null;
+    if (this.props.lobbyData.canStart) {
+      output = (
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={this.props.startGame}
+          className={styles.Button}
+        >
+          start
+        </Button>
+      );
+    }
+
+    return (
+      <div className={styles.FullLobby}>
+        <Lobby data={this.props.lobbyData.lobby} />
+        {output}
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={this.props.leaveLobby}
+          className={styles.Button}
+        >
+          leave
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default FullLobby;
+const mapStateToProps = state => {
+  return { lobbyData: state.lobby.currentLobby };
+};
+
+export default connect(mapStateToProps, {
+  startLobbyPolling,
+  leaveLobby,
+  endLobbyPolling,
+  startGame,
+})(FullLobby);
