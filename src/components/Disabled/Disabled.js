4548373087aa4fdcf58d1cdf937b941f1ff73784
Frostack <soulburn007@gmail.com>
2020-06-03 11:54:15 +0430

    🧼 clean up

--- src/components/Disabled/Disabled.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Disabled.module.css';
-
-function Disabled(props) {
-  return props.is ? <div className={styles.Disabled}></div> : null;
-}
-
-export default Disabled;
