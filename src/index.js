4aee7b4e6a0e1fe7843333ead8abc292bdc04d5e
Frostack <soulburn007@gmail.com>
2020-06-03 11:55:09 +0430

    🌱 add root index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <CssBaseline />
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
