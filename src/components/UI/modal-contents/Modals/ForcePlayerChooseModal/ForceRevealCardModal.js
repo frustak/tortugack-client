9e3f53bdb2e981e203c51abaafb9dc57d4a84377
Frostack <soulburn007@gmail.com>
2020-06-09 19:19:01 +0430

    ♻ revert back to cra

--- src/components/UI/modal-contents/Modals/ForcePlayerChooseModal/ForceRevealCardModal.js
+++ src/components/UI/modal-contents/Modals/ForcePlayerChooseModal/ForceRevealCardModal.js
@@ -5,7 +5,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import EventCard from '../../ViewTwoEventCardsModal/EventCard';
 import { sendGameAction, handleModal } from '../../../../../actions';
-import RevealedEventCardsModal from '../../RevealedEventCardsModal/RevealedEventCardsModal';
 
 const useStyles = makeStyles({
   root: {
